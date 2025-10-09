73e18df948962373f8df1acf2239b163076b0123
lamadev7 <parbatlama70@gmail.com>
2025-10-09 21:35:08 +0545

    Fix: handle cors in itty-router

--- worker/worker.ts
+++ worker/worker.ts
@@ -1,17 +1,20 @@
 import { handleUnfurlRequest } from 'cloudflare-workers-unfurl'
-import { AutoRouter, error, IRequest } from 'itty-router'
+import { AutoRouter, error, IRequest, cors } from 'itty-router'
 import { handleAssetDownload, handleAssetUpload } from './assetUploads'
-
 // make sure our sync durable object is made available to cloudflare
 export { TldrawDurableObject } from './TldrawDurableObject'
 
 // we use itty-router (https://itty.dev/) to handle routing. in this example we turn on CORS because
 // we're hosting the worker separately to the client. you should restrict this to your own domain.
+const { corsify } = cors({
+	origin: '*',
+});
 const router = AutoRouter<IRequest, [env: Env, ctx: ExecutionContext]>({
 	catch: (e) => {
 		console.error(e)
 		return error(e)
 	},
+	finally: [corsify],
 })
 	// requests to /connect are routed to the Durable Object, and handle realtime websocket syncing
 	.get('/api/connect/:roomId', (request, env) => {
